51d46d080f625e67248dab1a9984b471068f5c76
Atif Aziz <code@raboof.com>
2016-12-20 18:44:12 +0100

    Building & packaging scripts

---
+++ build.cmd
@@ -0,0 +1,27 @@
+@echo off
+pushd "%~dp0"
+call :main
+popd
+goto :EOF
+
+:main
+setlocal
+set DOTNETEXE=
+for %%f in (dotnet.exe) do set DOTNETEXE=%%~dpnx$PATH:f
+if not defined DOTNETEXE set DOTNETEXE=%ProgramFiles%\dotnet
+if not exist "%DOTNETEXE%" (
+    echo>&2 .NET Core does not appear to be installed on this machine, which is
+    echo>&2 required to build the solution. You can install it from the URL below
+    echo>&2 and then try building again:
+    echo>&2 https://dot.net
+    exit /b 1
+)
+"%DOTNETEXE%" --info ^
+  && "%DOTNETEXE%" restore ^
+  && call :build Debug ^
+  && call :build Release
+goto :EOF
+
+:build
+"%DOTNETEXE%" build -c %1 src\NSocialCalcSave
+goto :EOF
---
+++ pack.cmd
@@ -0,0 +1,22 @@
+@echo off
+pushd "%~dp0"
+call :main %*
+popd
+goto :EOF
+
+:main
+setlocal
+set DOTNETEXE=
+for %%f in (dotnet.exe) do set DOTNETEXE=%%~dpnx$PATH:f
+if not defined DOTNETEXE set DOTNETEXE=%ProgramFiles%\dotnet
+if not exist "%DOTNETEXE%" (
+    echo>&2 .NET Core does not appear to be installed on this machine, which is
+    echo>&2 required to build the solution. You can install it from the URL below
+    echo>&2 and then try building again:
+    echo>&2 https://dot.net
+    exit /b 1
+)
+set VERSION_SUFFIX=
+if not "%~1"=="" set VERSION_SUFFIX=--version-suffix %1
+call build && "%DOTNETEXE%" pack -c Release %VERSION_SUFFIX% src\NSocialCalcSave
+goto :EOF
